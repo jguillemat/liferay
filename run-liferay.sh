013e99dffde9e763be646f8c8e1a6330126ebb00
jguillemat <jguillemat@users.noreply.github.com>
2018-07-19 12:45:46 +0200

    Update run-liferay.sh

--- run-liferay.sh
+++ run-liferay.sh
@@ -33,6 +33,9 @@ For Liferay container run, you must either specify the following environment
 variables:
   POSTGRESQL_USER (regex: '$psql_identifier_regex')
   POSTGRESQL_PASSWORD (regex: '$psql_password_regex')
+  POSTGRESQL_SERVICE_HOST 
+  POSTGRESQL_SERVICE_PORT 
+  POSTGRESQL_DATABASE 
 Or both.
 EOF
   exit 1
@@ -40,12 +43,11 @@ EOF
 
 function check_env_vars() {
 
-  if [[ -v POSTGRESQL_USER || -v POSTGRESQL_PASSWORD ]]; then
+  if [[ -v POSTGRESQL_USER || -v POSTGRESQL_PASSWORD || -v POSTGRESQL_SERVICE_HOST || -v POSTGRESQL_SERVICE_PORT || -v POSTGRESQL_DATABASE]]; then
     # one var means all three must be specified
     [[ -v POSTGRESQL_USER && -v POSTGRESQL_PASSWORD ]] || usage
     [[ "$POSTGRESQL_USER"     =~ $psql_identifier_regex ]] || usage
     [[ "$POSTGRESQL_PASSWORD" =~ $psql_password_regex   ]] || usage
-
     [ ${#POSTGRESQL_USER} -le 63 ] || usage "PostgreSQL username too long (maximum 63 characters)"
     postinitdb_actions+=",simple_db"
   fi
@@ -80,48 +82,27 @@ prepare_liferay_portal_properties() {
 
 prepare_liferay_tomcat_config() {
   
-  echo "
-   Configuring Tomcat server.xml ...
-  "
-
-  if [ -v POSTGRESQL_URL ]; then
-	  POSTGRESQL_URL="jdbc:postgresql://$POSTGRESQL_SERVICE_HOST:$POSTGRESQL_SERVICE_PORT/$POSTGRESQL_DATABASE"
-  fi
-
-  sed -i 's/POSTGRESQL_USER/'"$POSTGRESQL_USER"'/g' $LIFERAY_HOME/server.xml_template
-  sed -i 's/POSTGRESQL_PASSWORD/'"$POSTGRESQL_PASSWORD"'/g' $LIFERAY_HOME/server.xml_template
-  sed -i 's/POSTGRESQL_URL/'"$POSTGRESQL_URL"'/g' $LIFERAY_HOME/server.xml_template
+  echo "Configuring Tomcat server.xml ..."
 
   cp $LIFERAY_HOME/server.xml_template  $CATALINA_HOME/conf/server.xml
 
-  echo "
-  Continuing.
-  "
+  sed -i 's/POSTGRESQL_USER/'"$POSTGRESQL_USER"'/g' $CATALINA_HOME/conf/server.xml
+  sed -i 's/POSTGRESQL_PASSWORD/'"$POSTGRESQL_PASSWORD"'/g' $CATALINA_HOME/conf/server.xml
+  sed -i 's/POSTGRESQL_SERVICE_HOST/'"$POSTGRESQL_SERVICE_HOST"'/g' $CATALINA_HOME/conf/server.xml
+  sed -i 's/POSTGRESQL_SERVICE_PORT/'"$POSTGRESQL_SERVICE_PORT"'/g' $CATALINA_HOME/conf/server.xml
+  sed -i 's/POSTGRESQL_DATABASE/'"$POSTGRESQL_DATABASE"'/g' $CATALINA_HOME/conf/server.xml
+
+  echo "Continuing."
 }
 
 run_portal() {
-
   set -e
-
-  # Drop root privileges if we are running liferay
-  # allow the container to be started with `--user`
-  if [ "$1" = 'catalina.sh' -a "$(id -u)" = '0' ]; then
-    # Change the ownership of Liferay Shared Volume to liferay
-
-    if [[ ! -d "$LIFERAY_SHARED" ]]; then
-      mkdir -p $LIFERAY_SHARED
-    fi
-
-    chown -R liferay:liferay $LIFERAY_SHARED
-    chown -R liferay:liferay $LIFERAY_HOME
-    
-    set -- gosu liferay "$@"
-  fi
-
-  # As argument is not related to liferay,
-  # then assume that user wants to run his own process,
-  # for example a `bash` shell to explore this image
-  exec "$@"
+  cd $CATALINA_HOME
+  ./bin/catalina.sh run
 }
 
 main "$@"
+
+
+
+
