c1363fc302e6122dc170b4830c0706392cf88228
jguillemat <jguillemat@users.noreply.github.com>
2018-07-20 10:22:56 +0200

    Add files via upload

--- tomcat_config/setenv.sh
+++ tomcat_config/setenv.sh
@@ -1,1 +1,4 @@
-CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms1g -Xmx4g -XX:NewSize=200m -XX:MaxNewSize=200m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+CATALINA_OUT=$CATALINA_HOME/logs/catalina-$HOSTNAME.out
+
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Dcompany-id-properties=true -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=CET -Xms2048m -Xmx2048m -XX:MaxPermSize=512m  -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8686 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dhttp.proxyHost=193.16.43.201 -Dhttp.proxyPort=3127 -Dhttp.nonProxyHosts='localhost|rest-services|oauth-services|internal_http|des.www.ancert.com|des.agn.notariado.org'"
+
